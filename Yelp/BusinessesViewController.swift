99ef16a2968790ad0d8a546e7d40412c4949e2ca
zskyfly <zac@lyft.com>
2016-02-18 17:41:33 -0800

    autolayout constraints

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -19,6 +19,8 @@ class BusinessesViewController: UIViewController {
 
         tableView.delegate = self
         tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
 
         Business.searchWithTerm("Thai", completion: { (businesses: [Business]!, error: NSError!) -> Void in
             self.businesses = businesses
